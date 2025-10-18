0f7ee05ad50918c8b8745a9d84d854145c47ee30
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2025-10-18 20:53:14 +0900

    feat: google login ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import { UserProfile } from "@/components/daydot/layout/Header/UserProfile";
 import { Providers } from "./providers";
 
